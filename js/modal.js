f6ef3900f5d95848b9a2d7ad81cab39dd12d853c
GRACIEST <63868635+websurfers@users.noreply.github.com>
2023-03-11 21:49:04 +0500

    Add files via upload

---
+++ js/modal.js
@@ -0,0 +1,27 @@
+// info.php avatar Модальное
+
+var modal = document.getElementById('myModal');
+
+
+var btn = document.getElementById("Avatar");
+
+
+var span = document.getElementsByClassName("close")[0];
+
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
